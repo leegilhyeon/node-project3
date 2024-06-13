b738a4be110830cf5a80a1234ca7add9df3633ef
leegilhyeon <164996803+leegilhyeon@users.noreply.github.com>
2024-06-14 02:33:57 +0900

    이력서 리팩토링

--- src/controllers/resumes.controller.js
+++ src/controllers/resumes.controller.js
@@ -1,33 +1,105 @@
-import { ResumesService } from "../services/resumes.service.js";
+import { HTTP_STATUS } from "../constants/http-status.constant.js";
+import { MESSAGES } from "../constants/message.constant.js";
 
 export class ResumesController {
-    resumesService = new ResumesService();
-
+    constructor(resumesService) {
+        this.resumesService = resumesService;
+    }
+    //이력서 생성
     createResume = async (req, res, next) => {
         try{
-            const { title, content } = req.body;
             const user = req.user;
-            const authorId = user.id;
-
-            const createResume = await this.resumesService.createResume(
-                authorId,
-                title,
-                content,
-            );
-
-            return res.status(201).json({ data: createResume });
+            const { title, content } = req.body;
+             const authorId = user.id;
+            const resume = await this.resumesService.createResume(authorId, title,content);
+        
+            return res.status(HTTP_STATUS.CREATED).json({
+              status: HTTP_STATUS.CREATED,
+              message: MESSAGES.RESUMES.CREATE.SUCCEED,
+              data: resume,
+            });
         } catch(err) {
             next(err);
         }
     }
-
+    //이력서 목록 조회
     getResumes = async (req, res, next) => {
         try{
-            const resumes = await this.resumesService.findManyResumes();
-            
-            return res.status(200).json({ data: resumes });
-        } catch(err) {
-            next(err);
+            const user = req.user;
+            const authorId = user.id;
+            const { sort } = req.query;
+            const resumes = await this.resumesService.findResumes(authorId, sort);
+
+            return res.status(HTTP_STATUS.OK).json({
+              status: HTTP_STATUS.OK,
+              message: MESSAGES.RESUMES.READ_LIST.SUCCEED,
+              data: resumes,
+            });
+        } catch(error) {
+            next(error);
         }
     }
-}
+
+    //이력서 상세조회
+    getResume = async (req, res, next) => {
+        try {
+            const user = req.user;
+            const authorId = user.id;       
+            const { id } = req.params;
+            const resume = await this.resumesService.getResume(authorId, id);
+
+            if (!resume) {
+              return res.status(HTTP_STATUS.NOT_FOUND).json({
+                status: HTTP_STATUS.NOT_FOUND,
+                message: MESSAGES.RESUMES.COMMON.NOT_FOUND,
+              });
+            }
+
+            return res.status(HTTP_STATUS.OK).json({
+              status: HTTP_STATUS.OK,
+              message: MESSAGES.RESUMES.READ_DETAIL.SUCCEED,
+              data: resume,
+            });
+          } catch (error) {
+            next(error);
+          }
+    }
+    //이력서 수정
+    updateResume = async (req, res, next) => {
+        try {
+            const user = req.user;
+            const authorId = user.id;
+            const { id } = req.params;
+            const { title, content } = req.body;
+
+            const resume = await this.resumesService.updateResume(authorId, id, title, content)
+
+         return res.status(HTTP_STATUS.OK).json({
+          status: HTTP_STATUS.OK,
+          message: MESSAGES.RESUMES.UPDATE.SUCCEED,
+          data:resume,
+        });
+        } catch (error) {
+          next(error);
+        }
+    }
+  //이력서 삭제
+  deleteResume = async (req, res, next) => {
+    try {
+        const user = req.user;
+        const authorId = user.id;
+        const { id } = req.params;
+
+        const resume = await this.resumesService.deleteResume(authorId, id)
+    
+        return res.status(HTTP_STATUS.OK).json({
+          status: HTTP_STATUS.OK,
+          message: MESSAGES.RESUMES.DELETE.SUCCEED,
+          data: resume,
+        });
+      } catch (error) {
+        next(error);
+      }
+  }
+
+    }
